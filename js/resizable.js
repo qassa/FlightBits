666f32affce04e33b2f582d7fcc6ef66cba36737
GlebNaz <nazemnovgleb@gmail.com>
2018-08-22 18:19:59 +0400

    modal window and adding edit tools

--- js/resizable.js
+++ js/resizable.js
@@ -78,3 +78,53 @@ function FindByAttributeValue(attribute, value, element_type) {
 function numOfRecords() {
 
 }
+
+
+function ready() {
+    modalCoords();
+    initTools();
+}
+//ожидание полной прогрузки DOM
+document.addEventListener("DOMContentLoaded", ready);
+
+//перерасчет координат левого верхнего угла для блока modal_text
+function modalCoords() {
+    //найти блок с данными, определить его ширину
+    modalBlock = document.getElementById("modal_text");
+    modalWidth = modalBlock.clientWidth;
+    modalHeight = modalBlock.clientHeight;
+    //взять ширину доступную для отбражения в окне браузера
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    //определить координаты левой верхней точки
+    modalBlock.style.left = (windowWidth / 2) - (modalWidth / 2);
+    modalBlock.style.top = (windowHeight / 2) - (modalHeight / 2);
+
+    modalWidth = modalBlock.clientWidth;
+
+    //установить положение для элемента close_modal
+    modalClose = document.getElementById("close_modal");
+    modalClose.style.left = modalWidth - 30;
+    modalClose.addEventListener('click', function() {
+        (document.getElementById("modal_text")).style.display = "none";
+        (document.getElementById("fon")).style.display = "none";
+    });
+}
+
+function initTools() {
+    document.getElementById("modal_text").style.display = "none";
+    document.getElementById("fon").style.display = "none";
+
+    var elem = document.getElementsByClassName("add")[0];
+    elem.addEventListener('click', function() {
+        (document.getElementById("modal_text")).style.display = "block";
+        (document.getElementById("fon")).style.display = "block";
+    });
+
+    var elem = document.getElementsByClassName("edit")[0];
+    elem.addEventListener('click', function() {
+        (document.getElementById("modal_text")).style.display = "block";
+        (document.getElementById("fon")).style.display = "block";
+    });
+}
